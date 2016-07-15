d5806e63bef0a825cd944b7dfe257ff469e59022
horia-delicoti <horia.delicoti@gmail.com>
2016-07-15 14:05:46 +0300

    Removed hardcoded command

--- lib/resin_lib.py
+++ lib/resin_lib.py
@@ -5,10 +5,6 @@ from subprocess import call
 def open_app(name, type):
 	call(["resin", "app", "create", name, "--type", type])
 
-@keyword("Git clone text2speech")
-def git_clone_text2speech():
-	call(["git", "clone", "https://github.com/resin-io/text2speech.git"])
-
 @keyword("Add git remote resin")
 def add_remote_resin(path, git):
 	call(["git", "-C", path,  "remote", "add", "resin", git])
